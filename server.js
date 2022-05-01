a9f366f77b49b74aea7aa59672b7acd958298ecc
coderbennett <jbennett@csumb.edu>
2022-05-01 15:16:16 -0700

    comment function TODOs

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 
 const sysArt = require('./lib/sysArt');
-
-const Department = require('./lib/Department')
-const Role = require('./lib/Role');
-const Employee = require('./lib/Employee');
+const questions = require('./lib/questions');
 
 const db = mysql.createConnection(
     {
@@ -28,6 +25,33 @@ function startSys() {
 function mainMenu() {
     inquirer
         .prompt()
+}
+
+function addEmployee() {
+    //use this function to inquire user
+    //it should then insert the new employee into the db
+    //log "added first + last name to the database"
+    //lastly, open the main menu again
+}
+
+function addDept() {
+    //use this function to inquire user
+    //it should then insert the new deptartment into the db
+    //log "added dept to the database"
+    //open main menu
+}
+
+function addRole() {
+    //use this function to inquire user
+    //it should then insert the new role into the db
+    //log "added role to the database"
+    //open main menu
+}
+
+function viewTable(table) {
+    //use mysql query to show table user wants to see
+    //display the table
+    //open main menu
 }
 
 // console.table([{
