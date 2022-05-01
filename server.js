41468493908e4e91fdc66000ffdd070b2071db6d
coderbennett <jbennett@csumb.edu>
2022-05-01 14:26:12 -0700

    add ascii art, add package.json

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const SysArt = require('./lib/SysArt');
+const Department = require('./lib/Department')
+const Role = require('./lib/Role');
+const Employee = require('./lib/Employee');
+
+function startSys() {
+    console.log(SysArt);
+}
+
 
 // console.table([{
 //     name: 'foo',
@@ -15,3 +24,6 @@ const cTable = require('console.table');
 // ---- ---
 // foo 10
 // bar 20
+
+            
+startSys();
