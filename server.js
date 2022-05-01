3ce0f09ad61e26bb7b8914a5740be2f34dd7a370
coderbennett <jbennett@csumb.edu>
2022-05-01 12:25:36 -0700

    add dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+// console.table([{
+//     name: 'foo',
+//     age: 10
+// }, {
+//     name: 'bar',
+//     age: 20
+// }]);
+
+// prints
+// name age
+// ---- ---
+// foo 10
+// bar 20
